64177f44949de656ade50065340053cb9d75305e
Hansen Tanio <hansentanio33@gmail.com>
2023-05-01 09:30:36 +0700

    initial page

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatefulWidget {
+  const InitialPage({super.key});
+
+  @override
+  State<InitialPage> createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            "lib/images/Let's Connect Together.png",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Let\'s Connect Together',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 300,
+            height: 60,
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(width: 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100), // <-- Radius
+                ),
+              ),
+              onPressed: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => const LoginPage()));
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 300,
+            height: 60,
+            child: ElevatedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100), // <-- Radius
+                ),
+              ),
+              onPressed: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => const SignUpScreen()));
+              },
+              child: const Text(
+                'Sign In',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
