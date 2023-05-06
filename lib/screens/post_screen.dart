1d04b3b7d0f0ab94b4b8aee80e994fd35a717a54
Victor <v39309@gmail.com>
2023-05-06 16:09:12 +0700

    Membuat preview foto dan textfield deskripsi

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:hexagram/components/my_textfield.dart';
 
 class PostScreen extends StatelessWidget {
-  const PostScreen({super.key});
+  final descriptionController = TextEditingController();
+  PostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +30,22 @@ class PostScreen extends StatelessWidget {
               Icons.arrow_circle_right_outlined,
               size: 30,
             ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            child: Image.asset('lib/images/random-photos/10.jpg'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          MyTextField(
+            controller: descriptionController,
+            hintText: 'Add a Description',
+            obsecureText: false,
           ),
         ],
       ),
