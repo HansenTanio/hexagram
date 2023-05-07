844ec72c26cf46061ced7dedb64c6b49e4f2dc3b
Hansen Tanio <hansentanio33@gmail.com>
2023-05-07 13:00:57 +0700

    Last Commit

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:hexagram/components/my_switch.dart';
 import 'package:hexagram/initial_screens/initial_screen.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool switch01 = false;
+  bool switch02 = false;
+  bool switch03 = false;
+  bool switch04 = false;
+
+  void toggleSwitch01(bool value) {
+    setState(() {
+      switch01 = !switch01;
+      switch02 = false;
+    });
+  }
+
+  void toggleSwitch02(bool value) {
+    setState(() {
+      switch02 = !switch02;
+      switch01 = false;
+    });
+  }
+
+  void toggleSwitch03(bool value) {
+    setState(() {
+      switch03 = !switch03;
+    });
+  }
+
+  void toggleSwitch04(bool value) {
+    setState(() {
+      switch04 = !switch04;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,146 +57,155 @@ class SettingsScreen extends StatelessWidget {
         ),
         automaticallyImplyLeading: false,
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 40,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Image.asset('lib/images/icons/paintbrush.png'),
-              ),
-              Text(
-                'Theme',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 80),
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text(
-                      'White Mode',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Image.asset('lib/images/icons/paintbrush.png'),
                 ),
-                Row(
-                  children: [
-                    Text(
-                      'Dark Mode',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
+                Text(
+                  'Theme',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
               ],
             ),
-          ),
-          Row(
-            children: [
-              Container(
-                width: 40,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Image.asset('lib/images/icons/notification.png'),
+            Container(
+              margin: const EdgeInsets.only(left: 80),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      MySwitch(
+                        switchText: 'Light Mode',
+                        switchFunc: toggleSwitch01,
+                        switchValue: switch01,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      MySwitch(
+                        switchText: 'Dark Mode',
+                        switchFunc: toggleSwitch02,
+                        switchValue: switch02,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                'Notification',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 80),
-            child: Column(
+            ),
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text(
-                      'Jeda Notifikasi',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Image.asset('lib/images/icons/notification.png'),
                 ),
+                Text(
+                  'Notification',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
               ],
             ),
-          ),
-          Row(
-            children: [
-              Container(
-                width: 40,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Image.asset('lib/images/icons/padlock.png'),
+            Container(
+              margin: const EdgeInsets.only(left: 80),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      MySwitch(
+                        switchText: 'Jeda Notifikasi',
+                        switchFunc: toggleSwitch03,
+                        switchValue: switch03,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                'Privacy',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 80),
-            child: Column(
+            ),
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text(
-                      'Akun Privat',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Image.asset('lib/images/icons/padlock.png'),
                 ),
+                Text(
+                  'Privacy',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
               ],
             ),
-          ),
-          Row(
-            children: [
-              Container(
-                width: 40,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Image.asset('lib/images/icons/info.png'),
+            Container(
+              margin: const EdgeInsets.only(left: 80),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      MySwitch(
+                        switchText: 'Akun Privat',
+                        switchFunc: toggleSwitch04,
+                        switchValue: switch04,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                'Help',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                width: 40,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Image.asset('lib/images/icons/question.png'),
-              ),
-              Text(
-                'About',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => InitialScreen(),
-                ),
-              );
-            },
-            child: Text(
-              'Log out',
-              style: TextStyle(fontSize: 20),
             ),
-          )
-        ],
+            Row(
+              children: [
+                Container(
+                  width: 40,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Image.asset('lib/images/icons/info.png'),
+                ),
+                Text(
+                  'Help',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 40,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Image.asset('lib/images/icons/question.png'),
+                ),
+                Text(
+                  'About',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InitialScreen(),
+                  ),
+                );
+              },
+              child: Text(
+                'Log out',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
       ),
     );
   }
