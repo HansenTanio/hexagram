437e2f5b4bc32e77d129a2f18fbb5b449555d13e
Victor <v39309@gmail.com>
2023-06-29 16:10:04 +0700

    menambahkan provider untuk card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hexagram/initial_screens/initial_screen.dart';
 import 'package:hexagram/provider/account_provider.dart';
+import 'package:hexagram/provider/card_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AccountProvider())],
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AccountProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CardProvider(),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:hexagram/components/my_avatar.dart';
 import 'package:hexagram/components/my_card.dart';
+import 'package:hexagram/provider/card_provider.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    Future.microtask(() {
+      Provider.of<CardProvider>(context, listen: false).initialData();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final cardProv = Provider.of<CardProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -40,48 +56,56 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
-            MyCard(
-              accountName: 'Lisa',
-              account: '@lalalalisa_m',
-              profileImage: 'lib/images/profile/Lisa Profile.jpg',
-              postImage: 'lib/images/random-photos/6.jpg',
-              postTime: '20 min ago',
-              postDesc: 'Birthday week',
-            ),
-            MyCard(
-              accountName: 'New Jeans',
-              account: '@newjeans_official',
-              profileImage: 'lib/images/profile/New Jeans Profile.jpg',
-              postImage: 'lib/images/random-photos/29.jpg',
-              postTime: '35 min ago',
-              postDesc: 'NewJeans \'Zero\' Available Now',
-            ),
-            MyCard(
-              accountName: 'Toshiro Nishio',
-              account: '@libertywalk_toshi',
-              profileImage: 'lib/images/profile/Libertywalk Toshi Profile.jpg',
-              postImage: 'lib/images/random-photos/13.jpg',
-              postTime: '50 min ago',
-              postDesc: 'LB WORKS Ferrari F40 in Japan',
-            ),
-            MyCard(
-              accountName: 'Kitsune',
-              account: '@kitsunee.__',
-              profileImage: 'lib/images/profile/Kitsune Profile.jpg',
-              postImage: 'lib/images/random-photos/14.jpg',
-              postTime: '1 hour ago',
-              postDesc: '#Kitsune',
-            ),
-            MyCard(
-              accountName: 'JISOO',
-              account: '@sooyaaa__',
-              profileImage: 'lib/images/profile/Jisoo Profile.jpg',
-              postImage: 'lib/images/random-photos/7.jpg',
-              postTime: '2 hours ago',
-              postDesc: '久しぶりに日本でBLINKに会えて幸せでした！',
-            ),
+            cardList(context)
           ],
         ),
+      ),
+    );
+  }
+}
+
+cardList(BuildContext context) {
+  final cardProv = Provider.of<CardProvider>(context);
+  if (cardProv.data == null) {
+    return Center(child: const Text('Tidak ada Data'));
+  } else {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyAvatar(
+                userName: 'Hansen',
+                imagePath: 'lib/images/profile/Hansen Profile.jpg',
+              ),
+              MyAvatar(
+                userName: 'Louis',
+                imagePath: 'lib/images/profile/Louis Profile.jpg',
+              ),
+              MyAvatar(
+                userName: 'Victor',
+                imagePath: 'lib/images/profile/Victor Profile.jpg',
+              ),
+            ],
+          ),
+          ListView(
+            children: List.generate(
+              cardProv.card['data']!.length,
+              (index) {
+                var item = cardProv.card['data']![index];
+                return MyCard(
+                  profileImage: item['profileImage'].toString(),
+                  accountName: item['accountName'].toString(),
+                  account: item['account'].toString(),
+                  postImage: item['postImage'].toString(),
+                  postTime: item['postTime'].toString(),
+                  postDesc: item['postDesc'].toString(),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
