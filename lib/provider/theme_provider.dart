7a9dfedbfbd2f814ee28a26778f2b027294bcdeb
Hansen Tanio <hansentanio33@gmail.com>
2023-06-29 09:37:57 +0700

    Menambahkan provider untuk tema

---
+++ lib/provider/theme_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  var light = ThemeData(
+    brightness: Brightness.light,
+  );
+
+  var dark = ThemeData(
+    brightness: Brightness.dark,
+  );
+
+  bool _enableDarkMode = false;
+  bool get enableDarkMode => _enableDarkMode;
+
+  set setBrightness(val) {
+    _enableDarkMode = val;
+    notifyListeners();
+  }
+}
