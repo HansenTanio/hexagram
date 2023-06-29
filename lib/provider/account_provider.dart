ef65678c0b4444f7558ec8c309f90d534e5aa769
Victor <v39309@gmail.com>
2023-06-29 18:48:30 +0700

    menambahkan data foto untuk halaman profil

--- lib/provider/account_provider.dart
+++ lib/provider/account_provider.dart
@@ -12,6 +12,23 @@ class AccountProvider extends ChangeNotifier {
       "profil": "lib/images/profile/Hansen Profile.jpg",
       "username": "hansentanio33",
       "password": "hansen123",
+      "photo": [
+        'lib/images/random-photos/1.jpg',
+        'lib/images/random-photos/25.jpg',
+        'lib/images/random-photos/48.jpg',
+        'lib/images/random-photos/11.jpg',
+        'lib/images/random-photos/4.jpg',
+        'lib/images/random-photos/19.jpg',
+        'lib/images/random-photos/9.jpg',
+        'lib/images/random-photos/18.jpg',
+        'lib/images/random-photos/22.jpg',
+        'lib/images/random-photos/33.jpg',
+        'lib/images/random-photos/32.jpg',
+        'lib/images/random-photos/27.jpg',
+        'lib/images/random-photos/37.jpg',
+        'lib/images/random-photos/43.jpg',
+        'lib/images/random-photos/47.jpg',
+      ]
     },
   };
   final louis = {
@@ -20,6 +37,10 @@ class AccountProvider extends ChangeNotifier {
       "profil": "lib/images/profile/Louis Profile.jpg",
       "username": "louislie57",
       "password": "louis123",
+      "photo": [
+        'lib/images/random-photos/20.jpg',
+        'lib/images/random-photos/30.jpg',
+      ]
     },
   };
   final victor = {
@@ -28,6 +49,13 @@ class AccountProvider extends ChangeNotifier {
       "profil": "lib/images/profile/Victor Profile.jpg",
       "username": "v39309",
       "password": "victor123",
+      "photo": [
+        'lib/images/random-photos/16.jpg',
+        'lib/images/random-photos/31.jpg',
+        'lib/images/random-photos/38.jpg',
+        'lib/images/random-photos/35.jpg',
+        'lib/images/random-photos/34.jpg',
+      ]
     },
   };
   dynamic _data;
